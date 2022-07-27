50a3561895ef96abf2791e9c2643b99659964c51
BJA Development <bjamien.development@gmail.com>
2022-07-28 06:27:43 +0700

    Card Dashboard

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -30,6 +30,7 @@
     $("#sidebarToggleTop").on('click', function(e) {
         $("body").toggleClass("sidebar-toggled");
         $(".sidebar").toggleClass("toggled");
+        $(".topbar").toggleClass("toggled");
         if ($(".sidebar").hasClass("toggled")) {
           $('.sidebar .collapse').collapse('hide');
         };
